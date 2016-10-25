cf4b757fb06b3895c41729cf406d4146f616a511
mariano240 <mariano24.palacios@gmail.com>
2016-10-25 16:56:43 -0300

    completado de atributos inmuebles (getters y setters tambien)

--- FRSF_Inmobiliaria/src/Clases/Inmueble.java
+++ FRSF_Inmobiliaria/src/Clases/Inmueble.java
@@ -11,7 +11,206 @@ import javax.persistence.Id;
 @Entity
 public class Inmueble implements Serializable {
     
+    private String calle;
+    private int numero;
+    private String piso;
+    private String departamento;   
+                                //Los atributos Localidad y Provincia se usan como int
+    private int localidad;      //para facilitar el get y set de combobox en las diferentes interfaces sin
+    private int provincia;      //tener que estar accediendo a la bd para ver textos equivalentes a posiciones
+    private String barrio;
+    private boolean propHoriz;
+    private float supInmueble;
+    private int antiguedad;
+    private int dormitorios;
+    private int banios;
+    private boolean cochera;
+    private boolean patio;
+    private boolean piscina;
+    private boolean aguaCorriente;
+    private boolean cloacas;
+    private boolean gasNatural;
+    private boolean telefono;
+    private boolean lavadero;
+    private boolean pavimento;
+    private String comentarios;
 
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPiso() {
+        return piso;
+    }
+
+    public void setPiso(String piso) {
+        this.piso = piso;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public int getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(int localidad) {
+        this.localidad = localidad;
+    }
+
+    public int getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(int provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getBarrio() {
+        return barrio;
+    }
+
+    public void setBarrio(String barrio) {
+        this.barrio = barrio;
+    }
+
+    public boolean isPropHoriz() {
+        return propHoriz;
+    }
+
+    public void setPropHoriz(boolean propHoriz) {
+        this.propHoriz = propHoriz;
+    }
+
+    public float getSupInmueble() {
+        return supInmueble;
+    }
+
+    public void setSupInmueble(float supInmueble) {
+        this.supInmueble = supInmueble;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+
+    public int getDormitorios() {
+        return dormitorios;
+    }
+
+    public void setDormitorios(int dormitorios) {
+        this.dormitorios = dormitorios;
+    }
+
+    public int getBanios() {
+        return banios;
+    }
+
+    public void setBanios(int banios) {
+        this.banios = banios;
+    }
+
+    public boolean isCochera() {
+        return cochera;
+    }
+
+    public void setCochera(boolean cochera) {
+        this.cochera = cochera;
+    }
+
+    public boolean isPatio() {
+        return patio;
+    }
+
+    public void setPatio(boolean patio) {
+        this.patio = patio;
+    }
+
+    public boolean isPiscina() {
+        return piscina;
+    }
+
+    public void setPiscina(boolean piscina) {
+        this.piscina = piscina;
+    }
+
+    public boolean isAguaCorriente() {
+        return aguaCorriente;
+    }
+
+    public void setAguaCorriente(boolean aguaCorriente) {
+        this.aguaCorriente = aguaCorriente;
+    }
+
+    public boolean isCloacas() {
+        return cloacas;
+    }
+
+    public void setCloacas(boolean cloacas) {
+        this.cloacas = cloacas;
+    }
+
+    public boolean isGasNatural() {
+        return gasNatural;
+    }
+
+    public void setGasNatural(boolean gasNatural) {
+        this.gasNatural = gasNatural;
+    }
+
+    public boolean isTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(boolean telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isLavadero() {
+        return lavadero;
+    }
+
+    public void setLavadero(boolean lavadero) {
+        this.lavadero = lavadero;
+    }
+
+    public boolean isPavimento() {
+        return pavimento;
+    }
+
+    public void setPavimento(boolean pavimento) {
+        this.pavimento = pavimento;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+    
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
