c34422b3d900e966ea5e2ec37df7847914f28d05
mariano240 <mariano24.palacios@gmail.com>
2016-10-27 17:28:09 -0300

    mas logica...2

--- FRSF_Inmobiliaria/src/Clases/Inmueble.java
+++ FRSF_Inmobiliaria/src/Clases/Inmueble.java
@@ -11,205 +11,73 @@ import javax.persistence.Id;
 @Entity
 public class Inmueble implements Serializable {
     
-    private String calle;
-    private int numero;
-    private String piso;
-    private String departamento;   
-                                //Los atributos Localidad y Provincia se usan como int
-    private int localidad;      //para facilitar el get y set de combobox en las diferentes interfaces sin
-    private int provincia;      //tener que estar accediendo a la bd para ver textos equivalentes a posiciones
-    private String barrio;
-    private boolean propHoriz;
-    private float supInmueble;
-    private int antiguedad;
-    private int dormitorios;
-    private int banios;
-    private boolean cochera;
-    private boolean patio;
-    private boolean piscina;
-    private boolean aguaCorriente;
-    private boolean cloacas;
-    private boolean gasNatural;
-    private boolean telefono;
-    private boolean lavadero;
-    private boolean pavimento;
-    private String comentarios;
+    final private float supInmueble;
+    final private float supTerreno;
+    final private boolean Ac;
+    final private int Antiguedad;
+    final private int Bano;
+    final private String Barrio;
+    final private int CP;
+    final private String Calle;
+    final private String Depto;
+    final private int Dormitorio;
+    final private float Fondo;
+    final private float Frente;
+    final private boolean Garage;
+    final private boolean Gn;
+    final private boolean Lavadero;
+    final private int ListaFotos; // TODO ver como adjuntar fotos
+    final private int LocalidadIndice;
+    final private String LocalidadNombre;
+    final private int Numero;
+    final private String Observaciones;
+    final private int Orientacion;
+    final private String OtraLoc;
+    final private boolean Patio;
+    final private boolean Pavimento;
+    final private String Piso;
+    final private float Precio;
+    final private int Propietario; //TODO ver relacion en base de datos con hibernate
+    final private String ProvinciaNombre;
+    final private int ProvinciaIndice;
+    final private boolean Telefono;
+    final private int TipoInmueble;
 
-    public String getCalle() {
-        return calle;
-    }
-
-    public void setCalle(String calle) {
-        this.calle = calle;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public String getPiso() {
-        return piso;
-    }
-
-    public void setPiso(String piso) {
-        this.piso = piso;
-    }
-
-    public String getDepartamento() {
-        return departamento;
-    }
-
-    public void setDepartamento(String departamento) {
-        this.departamento = departamento;
-    }
-
-    public int getLocalidad() {
-        return localidad;
-    }
-
-    public void setLocalidad(int localidad) {
-        this.localidad = localidad;
-    }
-
-    public int getProvincia() {
-        return provincia;
-    }
-
-    public void setProvincia(int provincia) {
-        this.provincia = provincia;
-    }
-
-    public String getBarrio() {
-        return barrio;
-    }
-
-    public void setBarrio(String barrio) {
-        this.barrio = barrio;
-    }
-
-    public boolean isPropHoriz() {
-        return propHoriz;
-    }
-
-    public void setPropHoriz(boolean propHoriz) {
-        this.propHoriz = propHoriz;
-    }
-
-    public float getSupInmueble() {
-        return supInmueble;
-    }
-
-    public void setSupInmueble(float supInmueble) {
+    public Inmueble(float supInmueble, float supTerreno, boolean Ac, int Antiguedad, int Bano, String Barrio, int CP, String Calle, String Depto, int Dormitorio, float Fondo, float Frente, boolean Garage, boolean Gn, boolean Lavadero, int ListaFotos, int LocalidadIndice, String LocalidadNombre, int Numero, String Observaciones, int Orientacion, String OtraLoc, boolean Patio, boolean Pavimento, String Piso, float Precio, int Propietario, String ProvinciaNombre, int ProvinciaIndice, boolean Telefono, int TipoInmueble) {
         this.supInmueble = supInmueble;
+        this.supTerreno = supTerreno;
+        this.Ac = Ac;
+        this.Antiguedad = Antiguedad;
+        this.Bano = Bano;
+        this.Barrio = Barrio;
+        this.CP = CP;
+        this.Calle = Calle;
+        this.Depto = Depto;
+        this.Dormitorio = Dormitorio;
+        this.Fondo = Fondo;
+        this.Frente = Frente;
+        this.Garage = Garage;
+        this.Gn = Gn;
+        this.Lavadero = Lavadero;
+        this.ListaFotos = ListaFotos;
+        this.LocalidadIndice = LocalidadIndice;
+        this.LocalidadNombre = LocalidadNombre;
+        this.Numero = Numero;
+        this.Observaciones = Observaciones;
+        this.Orientacion = Orientacion;
+        this.OtraLoc = OtraLoc;
+        this.Patio = Patio;
+        this.Pavimento = Pavimento;
+        this.Piso = Piso;
+        this.Precio = Precio;
+        this.Propietario = Propietario;
+        this.ProvinciaNombre = ProvinciaNombre;
+        this.ProvinciaIndice = ProvinciaIndice;
+        this.Telefono = Telefono;
+        this.TipoInmueble = TipoInmueble;
     }
 
-    public int getAntiguedad() {
-        return antiguedad;
-    }
-
-    public void setAntiguedad(int antiguedad) {
-        this.antiguedad = antiguedad;
-    }
-
-    public int getDormitorios() {
-        return dormitorios;
-    }
-
-    public void setDormitorios(int dormitorios) {
-        this.dormitorios = dormitorios;
-    }
-
-    public int getBanios() {
-        return banios;
-    }
-
-    public void setBanios(int banios) {
-        this.banios = banios;
-    }
-
-    public boolean isCochera() {
-        return cochera;
-    }
-
-    public void setCochera(boolean cochera) {
-        this.cochera = cochera;
-    }
-
-    public boolean isPatio() {
-        return patio;
-    }
-
-    public void setPatio(boolean patio) {
-        this.patio = patio;
-    }
-
-    public boolean isPiscina() {
-        return piscina;
-    }
-
-    public void setPiscina(boolean piscina) {
-        this.piscina = piscina;
-    }
-
-    public boolean isAguaCorriente() {
-        return aguaCorriente;
-    }
-
-    public void setAguaCorriente(boolean aguaCorriente) {
-        this.aguaCorriente = aguaCorriente;
-    }
-
-    public boolean isCloacas() {
-        return cloacas;
-    }
-
-    public void setCloacas(boolean cloacas) {
-        this.cloacas = cloacas;
-    }
-
-    public boolean isGasNatural() {
-        return gasNatural;
-    }
-
-    public void setGasNatural(boolean gasNatural) {
-        this.gasNatural = gasNatural;
-    }
-
-    public boolean isTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(boolean telefono) {
-        this.telefono = telefono;
-    }
-
-    public boolean isLavadero() {
-        return lavadero;
-    }
-
-    public void setLavadero(boolean lavadero) {
-        this.lavadero = lavadero;
-    }
-
-    public boolean isPavimento() {
-        return pavimento;
-    }
-
-    public void setPavimento(boolean pavimento) {
-        this.pavimento = pavimento;
-    }
-
-    public String getComentarios() {
-        return comentarios;
-    }
-
-    public void setComentarios(String comentarios) {
-        this.comentarios = comentarios;
-    }
+    
     
     private static final long serialVersionUID = 1L;
     @Id
@@ -248,6 +116,6 @@ public class Inmueble implements Serializable {
         return "Clases.Inmueble[ id=" + id + " ]";
     }
     
-    public Inmueble(){
+    public void Inmueble(){
     }
 }
