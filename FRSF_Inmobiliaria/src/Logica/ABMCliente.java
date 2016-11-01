62e6d33bfd0ed38a0d6fcded4e31d106376f552e
GonzaloEtche <gonzalo.etche@gmail.com>
2016-11-01 15:25:06 -0300

    Alta Usuario checkeada

--- FRSF_Inmobiliaria/src/Logica/ABMCliente.java
+++ FRSF_Inmobiliaria/src/Logica/ABMCliente.java
@@ -9,23 +9,24 @@ import org.hibernate.Transaction;
 
 public class ABMCliente {
 
-    public static boolean usuarioExistente(String text, int selectedIndex) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ABMCliente() {
+    }
+    
+    public static boolean usuarioExistente(String numeroDocumento, int tipoDocumento) {
+        
+        return false;
     }
 
     public static void altaCliente(String nombre,String apellido,int tipoDoc, int numeroDoc,Provincia provincia, Localidad localidad, String domicilio, int alturaDomicilio, int telefono, String correo){
-        /*Cliente cliente = new Cliente(0, nombre, apellido, tipoDoc, numeroDoc, domicilio, alturaDomicilio, telefono, correo);
-        cliente.setIdProvincia(provincia);
-        cliente.setIdLocalidad(localidad);
+        Cliente cliente = new Cliente(localidad, provincia, nombre, apellido, 
+                                        tipoDoc, numeroDoc, domicilio, alturaDomicilio, telefono, correo);
         SessionFactory sesion = NewHibernateUtil.getSessionFactory();
         Session session;
         session = sesion.openSession();
         Transaction tx = session.beginTransaction();
         session.save(cliente);
         tx.commit();
-        session.close();*/
-    }
-    public ABMCliente() {
+        session.close();
     }
     
 }
