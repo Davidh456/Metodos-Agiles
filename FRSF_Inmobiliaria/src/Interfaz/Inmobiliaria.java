af1021c4f7f2686c51dde5093a2ae464a97f1306
GonzaloEtche <gonzalo.etche@gmail.com>
2016-11-02 17:09:51 -0300

    Se modifico la navegación, clase Inmobiliaria es singleton (para poder navegar) Se agrego menu principal, Se agregaron listados de Cliente/Propietario

---
+++ FRSF_Inmobiliaria/src/Interfaz/Inmobiliaria.java
@@ -0,0 +1,213 @@
+package Interfaz;
+
+/*Clase principal*/
+
+
+import Clases.Cliente;
+import Clases.Propietario;
+import Interfaz.*;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Inmobiliaria extends JFrame{
+    //Aqu� vamos a declarar todas las sub-pantallas que vamos a usar
+    private JPanel contentPane;
+    private JPanel fondo;
+    
+    //Esta parte de instancia es para hacer la clase Singleton
+    
+    private static Inmobiliaria instancia = null;
+    
+    JPanel AltaClienteOPropietario;
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					instancia = new Inmobiliaria();
+                                        instancia.inicializarInmobiliaria();
+                                        Inmobiliaria.getInstance().MenuPrincipal();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+    }
+    public static Inmobiliaria getInstance(){
+        if (instancia == null){
+                instancia = new Inmobiliaria();
+        }
+        return instancia;
+    }
+    Inmobiliaria(){
+        super();
+        instancia = this;
+    }
+    public void inicializarInmobiliaria(){
+        setVisible(true);
+        setResizable(false);
+        setIconImage(Toolkit.getDefaultToolkit().getImage(Inmobiliaria.class.getResource("/javax/swing/plaf/metal/icons/ocean/menu.gif")));
+        //Estas lineas son para crear el Frame Principal donde va a llamar a cada pantalla
+
+        //Aqu� que dan las dimensiones segun la resoluci�n de pantalla
+        Toolkit t = Toolkit.getDefaultToolkit();
+        java.awt.Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setBounds(0, 0, screenSize.width, screenSize.height-48);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        cerrar();
+
+        //Este contentPane es el JPanel que va a contener a toda la interfaz gr�fica
+        contentPane = new JPanel();
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        contentPane.setBounds(0, 0, screenSize.width, screenSize.height-48);
+
+        fondo = new JPanel();
+        fondo.setBounds(0, (contentPane.getHeight()/2)-(contentPane.getHeight()/4), contentPane.getWidth(), contentPane.getHeight()/2);
+        fondo.setBackground(new Color(190,190,250));
+        
+        // TODO 0 Aqu� vayan tirando los m�todos para ir probando las pantallas en la pantalla principal
+        
+
+    }
+    public void cerrar(){
+        this.addWindowListener(new WindowAdapter(){
+                public void windowClosing(WindowEvent e){
+                        int opcion = JOptionPane.showOptionDialog(null, 
+                                        "�Seguro que quiere salir?", "Cerrar", JOptionPane.YES_NO_OPTION, 2, null, new String[]{"Si","No"}, null);
+                        if (opcion == 0){
+					System.exit(0);
+				}
+				
+			}
+        });
+    }
+    public void AltaPropietario() {
+        contentPane.removeAll();
+        ABMClienteOPropietario pantallaABM = new ABMClienteOPropietario("propietario", "alta", null,null);
+        pantallaABM.setBounds((contentPane.getWidth()/2)-(pantallaABM.getWidth()/2), 
+							(contentPane.getHeight()/2)-(pantallaABM.getHeight()/2), 
+							pantallaABM.getWidth(), 
+							pantallaABM.getHeight());
+        contentPane.add(pantallaABM);
+        pantallaABM.setVisible(true);
+        contentPane.repaint();
+        contentPane.revalidate();
+    }
+    public void ModificacionPropietario(Propietario propietario) {
+        contentPane.removeAll();
+        ABMClienteOPropietario pantallaABM = new ABMClienteOPropietario("propietario", "modificacion", null,propietario);
+        pantallaABM.setBounds((contentPane.getWidth()/2)-(pantallaABM.getWidth()/2), 
+							(contentPane.getHeight()/2)-(pantallaABM.getHeight()/2), 
+							pantallaABM.getWidth(), 
+							pantallaABM.getHeight());
+        contentPane.add(pantallaABM);
+        pantallaABM.setVisible(true);
+        contentPane.repaint();
+        contentPane.revalidate();
+    }
+    public void BajaPropietario(Propietario propietario) {
+        contentPane.removeAll();
+        ABMClienteOPropietario pantallaABM = new ABMClienteOPropietario("propietario", "baja", null,propietario);
+        pantallaABM.setBounds((contentPane.getWidth()/2)-(pantallaABM.getWidth()/2), 
+							(contentPane.getHeight()/2)-(pantallaABM.getHeight()/2), 
+							pantallaABM.getWidth(), 
+							pantallaABM.getHeight());
+        contentPane.add(pantallaABM);
+        pantallaABM.setVisible(true);
+        contentPane.repaint();
+        contentPane.revalidate();
+    }
+    public void AltaCliente() {
+        contentPane.removeAll();
+        ABMClienteOPropietario pantallaABM = new ABMClienteOPropietario("cliente", "alta", null,null);
+        pantallaABM.setBounds((contentPane.getWidth()/2)-(pantallaABM.getWidth()/2), 
+							(contentPane.getHeight()/2)-(pantallaABM.getHeight()/2), 
+							pantallaABM.getWidth(), 
+							pantallaABM.getHeight());
+        contentPane.add(pantallaABM);
+        pantallaABM.setVisible(true);
+        contentPane.repaint();
+        contentPane.revalidate();
+    }
+    public void ModificacionCliente(Cliente cliente) {
+        contentPane.removeAll();
+        ABMClienteOPropietario pantallaABM = new ABMClienteOPropietario("cliente", "modificacion", cliente,null);
+        pantallaABM.setBounds((contentPane.getWidth()/2)-(pantallaABM.getWidth()/2), 
+                                                        (contentPane.getHeight()/2)-(pantallaABM.getHeight()/2), 
+                                                        pantallaABM.getWidth(), 
+                                                        pantallaABM.getHeight());
+        contentPane.add(pantallaABM);
+        pantallaABM.setVisible(true);
+        contentPane.repaint();
+        contentPane.revalidate();
+    }
+    public void BajaCliente(Cliente cliente) {
+        contentPane.removeAll();
+        ABMClienteOPropietario pantallaABM = new ABMClienteOPropietario("cliente", "baja", cliente,null);
+        pantallaABM.setBounds((contentPane.getWidth()/2)-(pantallaABM.getWidth()/2), 
+							(contentPane.getHeight()/2)-(pantallaABM.getHeight()/2), 
+							pantallaABM.getWidth(), 
+							pantallaABM.getHeight());
+        contentPane.add(pantallaABM);
+        pantallaABM.setVisible(true);
+        contentPane.repaint();
+        contentPane.revalidate();
+    }
+    public void AltaInmueble2() {
+        contentPane.removeAll();
+        AltaInmueble pantallaABM = new AltaInmueble();
+        pantallaABM.setBounds((contentPane.getWidth()/2)-(pantallaABM.getWidth()/2), 
+							(contentPane.getHeight()/2)-(pantallaABM.getHeight()/2), 
+							pantallaABM.getWidth(), 
+							pantallaABM.getHeight());
+        contentPane.add(pantallaABM);
+        pantallaABM.setVisible(true);
+        contentPane.repaint();
+        contentPane.revalidate();
+        
+    } 
+    public void ListarClientes(){
+        contentPane.removeAll();
+        ListarClientesOPropietarios pantallaListar = new ListarClientesOPropietarios("Clientes");
+        pantallaListar.setBounds((contentPane.getWidth()/2)-(pantallaListar.getWidth()/2), 
+							(contentPane.getHeight()/2)-(pantallaListar.getHeight()/2), 
+							pantallaListar.getWidth(), 
+							pantallaListar.getHeight());
+        contentPane.add(pantallaListar);
+        pantallaListar.setVisible(true);
+        contentPane.repaint();
+        contentPane.revalidate();
+        
+    }
+    public void ListarPropietarios(){
+        contentPane.removeAll();
+        ListarClientesOPropietarios pantallaListar = new ListarClientesOPropietarios("Propietarios");
+        pantallaListar.setBounds((contentPane.getWidth()/2)-(pantallaListar.getWidth()/2), 
+							(contentPane.getHeight()/2)-(pantallaListar.getHeight()/2), 
+							pantallaListar.getWidth(), 
+							pantallaListar.getHeight());
+        contentPane.add(pantallaListar);
+        pantallaListar.setVisible(true);
+        contentPane.repaint();
+        contentPane.revalidate();
+        
+    }
+    public void MenuPrincipal(){
+        contentPane.removeAll();
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.setBounds((contentPane.getWidth()/2)-(menu.getWidth()/2), 
+							(contentPane.getHeight()/2)-(menu.getHeight()/2), 
+							menu.getWidth(), 
+							menu.getHeight());
+        contentPane.add(menu);
+        menu.setVisible(true);
+        contentPane.repaint();
+        contentPane.revalidate();
+    }
+}
